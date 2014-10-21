716419852573b0c6742c88d1f78474d6100c3ddd
hacker1202 <hacker1202@hotmail.com>
2014-10-21 15:40:14 +0800

    2nd commit again

--- gen/com/example/youtubemap/R.java
+++ gen/com/example/youtubemap/R.java
@@ -1795,17 +1795,6 @@ containing a value of this type.
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-
-        <item name="android:windowActionBarOverlay">true</item>
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -1817,13 +1806,13 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050090;
+        public static final int AppBaseTheme=0x7f050092;
         /**  Application theme. 
          */
-        public static final int AppTheme=0x7f050091;
+        public static final int AppTheme=0x7f050090;
         /**  ActionBar theme. 
          */
-        public static final int AppTheme_ActionBar=0x7f050092;
+        public static final int AppTheme_ActionBar=0x7f050091;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
         public static final int TextAppearance_AppCompat_Base_CompactMenu_Dialog=0x7f050068;
